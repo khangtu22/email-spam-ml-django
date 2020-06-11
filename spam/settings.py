e1cee4a17d8caefebc312445c4299f00dd604c5b
khangtu22 <vankhang226@gmail.com>
2020-06-11 12:57:33 +0700

    update small

--- spam/settings.py
+++ spam/settings.py
@@ -126,7 +126,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
